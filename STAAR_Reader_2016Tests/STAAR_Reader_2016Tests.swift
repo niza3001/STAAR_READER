f14d1b762a4be2b19250db81b5d4dd17eb0c222b
niza3001 <n.zarei.3001@gmail.com>
2017-07-05 17:34:16 -0500

    Initial Commit

---
+++ STAAR_Reader_2016Tests/STAAR_Reader_2016Tests.swift
@@ -0,0 +1,36 @@
+//
+//  STAAR_Reader_2016Tests.swift
+//  STAAR_Reader_2016Tests
+//
+//  Created by niloofar zarei on 7/5/17.
+//  Copyright © 2017 TEILab. All rights reserved.
+//
+
+import XCTest
+@testable import STAAR_Reader_2016
+
+class STAAR_Reader_2016Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
